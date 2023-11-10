291723b765037b3f693428775efc498e33b8622f
Reminisce215 <121631934+Reminisce215@users.noreply.github.com>
2023-11-10 16:35:01 -0500

    -Updated flask docker image -Event data is now appended to a list -removed local directory specific functions -uuid may not be necessary

--- be-flask/dataTest.py
+++ be-flask/dataTest.py
@@ -8,18 +8,45 @@ time.sleep(3)
 url = 'http://localhost:5000/add_event'
 
 #Convert event image to base 64 so that it can be serialized by json
-with open("C:/Users/nican/Desktop/imageStorage/puppy.jpeg", "rb") as puppy_file:
-    encoded_puppy_img=base64.b64encode(puppy_file.read()).decode('utf-8')
+with open("C:/Users/nican/Desktop/imageStorage/puppy1.jpeg", "rb") as puppy_file1:
+    encoded_img=base64.b64encode(puppy_file1.read()).decode('utf-8')
 # Create dictionary of three key-value pairs to store time,event, and image respectively
 data = {
 
     'time': datetime.now().strftime('%I:%M:%S%p'),
     'event': 'Face Scanned',
-    'image': encoded_puppy_img
+    'image': encoded_img
 }
 
 # Send the data using a POST request
 response = requests.post(url, json=data)
 
+#Convert event image to base 64 so that it can be serialized by json
+with open("C:/Users/nican/Desktop/imageStorage/puppy2.jpg", "rb") as puppy_file2:
+    encoded_img=base64.b64encode(puppy_file2.read()).decode('utf-8')
+# Create dictionary of three key-value pairs to store time,event, and image respectively
+data = {
+
+    'time': datetime.now().strftime('%I:%M:%S%p'),
+    'event': 'Face Scanned',
+    'image': encoded_img
+}
+
+# Send the data using a POST request
+response = requests.post(url, json=data)
+
+#Convert event image to base 64 so that it can be serialized by json
+with open("C:/Users/nican/Desktop/imageStorage/puppy3.jpg", "rb") as puppy_file3:
+    encoded_img=base64.b64encode(puppy_file3.read()).decode('utf-8')
+# Create dictionary of three key-value pairs to store time,event, and image respectively
+data = {
+
+    'time': datetime.now().strftime('%I:%M:%S%p'),
+    'event': 'Face Scanned',
+    'image': encoded_img
+}
+
+# Send the data using a POST request
+response = requests.post(url, json=data)
 # Print the response
 print(response.json())
--- be-flask/flaskAPI.py
+++ be-flask/flaskAPI.py
@@ -1,69 +1,53 @@
 from flask import Flask, render_template, jsonify, request, send_from_directory
-import base64
-import os
+from flask_cors import CORS
 import uuid
 
 app = Flask(__name__)
+CORS(app)
+# Global variable to stores persistent list of events
+event_data = []
 
-# Global variable to store the latest event data
-latest_event_data = None
+@app.route('/')
+def home():
+    print("HOME!")
 
-
-# ML file endpoint to push successful scan data
-@app.route('/add_event', methods=['GET', 'POST'])
+#ML file endpoint to push successful scan data
+@app.route('/add_event', methods=['POST'])
 def add_event():
-    # global variable to extract current event details for front end retrieval
-    global latest_event_data
+    """
+    #uuid generate unique id for each image
+    #unique_id=uuid.uuid4()
+    #image_filename= f"image_{unique_id.jpg}"
+    """
+    # global variable to store event details for front end retrieval
+    global event_data
 
-    # event data
+    #current event data from json payload
     data = request.json
     time = data.get('time')
     event = data.get('event')
     encoded_img = data.get('image')
 
-    # Generate a unique filename with the provided time and a UUID
-    unique_id = uuid.uuid4()
-    image_filename = f"image_{unique_id}.jpg"
-
-    # Convert back to bytes
-    image_data = base64.b64decode(encoded_img)
-
-    # Save with .jpg extension
-    image_path = os.path.join("./images", image_filename)
-    with open(image_path, "wb") as puppy_file:
-        puppy_file.write(image_data)
-
-    # Store the event data in the global variable
+    # store current event into dictionaries
     latest_event_data = {
         'time': time,
         'event': event,
-        'image_filename': image_filename
+        'image.jpg': encoded_img
     }
+    #appends latest event to event data list
+    event_data.append(latest_event_data)
 
     print(f"{event} at: {time}")
-    return jsonify({"message": "Event added successfully", "image_filename": image_filename})
+    return jsonify({"message": "Event added successfully"})
 
-# front-end endpoint to retrieve event data
+#front-end endpoint to retrieve event data
 
-
-@app.route('/get_latest_event', methods=['GET'])
+@app.route('/get_event_data', methods=['GET'])
 def get_latest_event():
-    global latest_event_data
-    if latest_event_data is None:
+    global event_data
+    if not event_data :
         return jsonify({"message": "No event data available"}), 404
-    return jsonify(latest_event_data)
-
-# These two functions are for localhost testing
-
-
-@app.route('/images/<filename>')
-def serve_image(filename):
-    return send_from_directory("C:/Users/nican/Desktop/eventImage", filename)
-
-
-@app.route('/route_imagey')
-def route_image():
-    return render_template('index.html', image_filename="newPuppy.jpg")
+    return jsonify(event_data)
 
 
 if __name__ == '__main__':
